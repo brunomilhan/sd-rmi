35bba7ec76a246226697b73926da741480f59efb
Bruno Milhan <brunomilhan2@gmail.com>
2016-10-07 23:15:41 +0000

    lint code

--- server-rmi/src/main/java/model/LibraryFines.java
+++ server-rmi/src/main/java/model/LibraryFines.java
@@ -7,24 +7,22 @@ import java.util.TimerTask;
 /**
  * Created by Bruno on 07/10/2016.
  */
-public class LibraryFines {
+class LibraryFines {
     private Timer timer;
-    private FineTimerTask fineTimerTask;
 
-    public LibraryFines(){
+    LibraryFines() {
         timer = new Timer();
     }
 
-    public void applyFine(Client client){
-        fineTimerTask = new FineTimerTask(client);
-        System.out.println(LibraryRules.TIME_PENALTY);
+    void applyFine(Client client) {
+        FineTimerTask fineTimerTask = new FineTimerTask(client);
         timer.schedule(fineTimerTask, LibraryRules.TIME_PENALTY);
     }
 
-    private class FineTimerTask extends TimerTask{
+    private class FineTimerTask extends TimerTask {
         Client client;
 
-        FineTimerTask(Client client){
+        FineTimerTask(Client client) {
             this.client = client;
         }
 
