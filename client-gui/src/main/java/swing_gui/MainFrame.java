46f306d292af8ae12594a0915426401936b5e344
Bruno Milhan <brunomilhan2@gmail.com>
2016-10-08 21:11:02 +0000

    add listeners

--- client-gui/src/main/java/swing_gui/MainFrame.java
+++ client-gui/src/main/java/swing_gui/MainFrame.java
@@ -1,12 +1,13 @@
 package swing_gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Bruno on 08/10/2016.
  */
 public class MainFrame {
-    private JTextField titleApp;
     private JPanel panelBtns;
     private JPanel panelInfos;
     private JButton btnList;
@@ -14,9 +15,44 @@ public class MainFrame {
     private JButton btnRenew;
     private JButton btnReturn;
     private JButton btnReserve;
-    private JTextField titleFunctions;
     private JList list1;
     private JPanel mainPanel;
+    private JLabel labelMethods;
+    private JLabel labelAvailable;
+    private JList list2;
+    private JLabel labelLoans;
+
+    public MainFrame() {
+        btnList.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnBorrow.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnRenew.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnReturn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnReserve.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     public JPanel getMainPanel() {
         return mainPanel;
