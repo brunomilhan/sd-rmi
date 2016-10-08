8e167a85399e527b3a1de6140612eb89ef85ae87
Bruno Milhan <brunomilhan2@gmail.com>
2016-10-08 18:28:33 +0000

    create basic jframe

--- client-gui/src/main/java/swing_gui/Main.java
+++ client-gui/src/main/java/swing_gui/Main.java
@@ -1,10 +1,17 @@
 package swing_gui;
 
+import javax.swing.*;
+
 /**
  * Created by Bruno on 07/10/2016.
  */
 public class Main {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Java RMI");
+        jFrame.setContentPane(new MainFrame().getMainPanel());
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setVisible(true);
 
     }
 }
---
+++ client-gui/src/main/java/swing_gui/MainFrame.java
@@ -0,0 +1,24 @@
+package swing_gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Bruno on 08/10/2016.
+ */
+public class MainFrame {
+    private JTextField titleApp;
+    private JPanel panelBtns;
+    private JPanel panelInfos;
+    private JButton btnList;
+    private JButton btnBorrow;
+    private JButton btnRenew;
+    private JButton btnReturn;
+    private JButton btnReserve;
+    private JTextField titleFunctions;
+    private JList list1;
+    private JPanel mainPanel;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
